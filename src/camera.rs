bf632396ed187f7f39d48b9a721420e9951dac9b
Francisco Soto <ebobby@ebobby.org>
2019-03-14 12:36:39 -0700

    Minor camera refactor.

--- src/camera.rs
+++ src/camera.rs
@@ -6,7 +6,7 @@ use super::vector::Vec3;
 /// The objct that defines where to look from and where to inside the scene.
 #[derive(Debug)]
 pub struct Camera {
-    origin: Vec3,
+    look_from: Vec3,
     corner: Vec3,
     horizontal: Vec3,
     vertical: Vec3,
@@ -20,29 +20,29 @@ impl Camera {
     ///
     /// # Arguments
     ///
-    /// * `origin` - Position of the camera in world space.
+    /// * `look_from` - Position of the camera in world space.
     /// * `look_at` - Where the camera is directly looking at.
     /// * `fov` - Field of view angle.
     /// * `aspect_ratio` - Aspect ratio of the image. Usually width/height.
     /// * `roll` - Angle of rotation on the z (view) coordinate (roll to a side).
     ///
-    pub fn new(origin: Vec3, look_at: Vec3, fov: f64, aspect_ratio: f64, roll: f64) -> Self {
+    pub fn new(look_from: Vec3, look_at: Vec3, fov: f64, aspect_ratio: f64, roll: f64) -> Self {
         let roll_angle = roll.to_radians();
         let rotated_up = Vec3::new(-roll_angle.sin(), roll_angle.cos(), 0.0);
 
-        let w = (origin - look_at).normalize();
+        let w = (look_from - look_at).normalize();
         let u = rotated_up.cross(w).normalize();
-        let v = w.cross(u).normalize();
+        let v = w.cross(u);
 
         let half_height = (fov.to_radians() / 2.0).tan();
         let half_width = half_height * aspect_ratio;
 
-        let corner = origin - (u * half_width) + (v * half_height) - w;
+        let corner = look_from - (u * half_width) + (v * half_height) - w;
         let horizontal = u * (2.0 * half_width);
         let vertical = -v * (2.0 * half_height);
 
         Camera {
-            origin,
+            look_from,
             corner,
             horizontal,
             vertical,
@@ -56,17 +56,17 @@ impl Camera {
     ///
     /// # Arguments
     ///
-    /// * `s` - horizontal screen coordinate.
-    /// * `t` - vertical screen coordinate.
+    /// * `u` - horizontal screen coordinate.
+    /// * `v` - vertical screen coordinate.
     ///
     /// *Note*: Screen coordinates are assumed to be between 0.0 and 1.0
     /// inclusive.
-    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
-        let direction = self.corner + (self.horizontal * s) + (self.vertical * t) - self.origin;
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        let direction = self.corner + self.horizontal * u + self.vertical * v - self.look_from;
 
         Ray {
-            origin: self.origin,
-            direction: direction.normalize(),
+            origin: self.look_from,
+            direction,
         }
     }
 }
