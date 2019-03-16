d96342f4dc0ec40db8e112ccc9bd7124b0502d36
Francisco Soto <ebobby@ebobby.org>
2019-03-16 16:05:03 -0700

    Added checker texture.

--- src/texture.rs
+++ src/texture.rs
@@ -1,28 +1,36 @@
 use crate::Color;
 use crate::Vec3;
 
+mod checker;
 mod constant_color;
 
+use checker::Checker;
 use constant_color::ConstantColor;
 
 /// Texture object.
 #[derive(Clone, Copy, Debug)]
 pub enum Texture {
     ConstantColor(ConstantColor),
+    Checker(Checker),
 }
 
 pub trait TextureObject {
     fn value(&self, u: f64, v: f64, p: Vec3) -> Color;
 }
 
-impl Texture  {
+impl Texture {
     pub fn constant_color(color: Color) -> Texture {
         Texture::ConstantColor(ConstantColor { color })
+    }
+
+    pub fn checker(squares: usize, odd: Color, even: Color) -> Texture {
+        Texture::Checker(Checker { squares, odd, even })
     }
 
     pub fn value(&self, u: f64, v: f64, p: Vec3) -> Color {
         match *self {
             Texture::ConstantColor(color) => color.value(u, v, p),
+            Texture::Checker(checker) => checker.value(u, v, p),
         }
     }
 }
